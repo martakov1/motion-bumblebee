f3cbd0e1ef4db6e09e936ecfb06bdbe15a329b15
martakov1 <63662599+martakov1@users.noreply.github.com>
2020-12-10 11:31:04 -0500

    idk

--- dev/js/dashboard.js
+++ dev/js/dashboard.js
@@ -8,9 +8,9 @@ const dashboardTL = gsap.timeline();
 
 export function dashboardAnimation(){
 
-    dashboardTL.from("#dashboard",{duration:2, alpha:0})
-    
-    .from("#path-29",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+    dashboardTL.from(".dashboard",{duration:2, alpha:0})
+
+    .from("#path-29",{duration:2, drawSVG:"0%", ease:"none"}, "dash")
     .from("path-31",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
     .from("#path-33",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
     .from("#path-35",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
@@ -22,6 +22,21 @@ export function dashboardAnimation(){
     .from("#path-55",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
     .from("#path-57",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
     .from("#path-61",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+    .from("#path-9",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+    .from("#path-11",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+    .from("#path-13",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+    .from("#path-15",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+    .from("#path-17",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+    .from("#path-19",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+    .from("#path-27",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+    .from("#path-29",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+    .from("#path-31",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+    .from("#path-33",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+    .from("#path-35",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+    .from("#path-37",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+    .from("#path-33",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+    .from("#path-33",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+    
     ;
     return dashboardTL;
 
