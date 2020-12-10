7397294f160ef658073f1a46d4280b96f3b29f13
martakov1 <63662599+martakov1@users.noreply.github.com>
2020-12-09 21:25:13 -0500

    draw

--- dev/js/dashboard.js
+++ dev/js/dashboard.js
@@ -12,15 +12,17 @@ export function dashboardAnimation(){
 
     return dashboardTL;
 
-    .from("#Path-6",{duration:2, drawSVG:"0%", ease:"none"}, "bumble-bee")
-    .from("#Path-10",{duration:2, drawSVG:"0%", ease:"none"}, "bumble-bee")
-    .from("#Path-13",{duration:2, drawSVG:"0%", ease:"none"}, "bumble-bee")
-    .from("#Path-14",{duration:2, drawSVG:"0%", ease:"none"}, "bumble-bee")
-    .from("#Path-16",{duration:2, drawSVG:"0%", ease:"none"}, "bumble-bee")
-    .from("#Path-18",{duration:2, drawSVG:"0%", ease:"none"}, "bumble-bee")
-    .from("#Path-21",{duration:2, drawSVG:"0%", ease:"none"}, "bumble-bee")
-    .from("#Path-23",{duration:2, drawSVG:"0%", ease:"none"}, "bumble-bee")
-    .from("#Path-24",{duration:2, drawSVG:"0%", ease:"none"}, "bumble-bee")
-    .from("#Path-25",{duration:2, drawSVG:"0%", ease:"none"}, "bumble-bee")
-    .from("#Path-28",{duration:2, drawSVG:"0%", ease:"none"}, "bumble-bee")
+    .from("#path-29",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+    .from("path-31",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+    .from("#path-33",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+    .from("#path-35",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+    .from("#path-37",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+    .from("#path-47",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+    .from("#path-49",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+    .from("#path-51",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+    .from("#path-53",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+    .from("#path-55",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+    .from("#path-57",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+    .from("#path-61",{duration:2, drawSVG:"0%", ease:"none"}, "bars")
+
 }
