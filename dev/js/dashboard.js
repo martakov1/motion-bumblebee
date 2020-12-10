1d6ac605b30a5920aa58c73146b2f5e88d4ad8bc
martakov1 <63662599+martakov1@users.noreply.github.com>
2020-12-09 21:05:33 -0500

    drop shadow

--- dev/js/dashboard.js
+++ dev/js/dashboard.js
@@ -12,5 +12,15 @@ export function dashboardAnimation(){
 
     return dashboardTL;
 
-    // test hello 22222
+    .from("#Path-6",{duration:2, drawSVG:"0%", ease:"none"}, "bumble-bee")
+    .from("#Path-10",{duration:2, drawSVG:"0%", ease:"none"}, "bumble-bee")
+    .from("#Path-13",{duration:2, drawSVG:"0%", ease:"none"}, "bumble-bee")
+    .from("#Path-14",{duration:2, drawSVG:"0%", ease:"none"}, "bumble-bee")
+    .from("#Path-16",{duration:2, drawSVG:"0%", ease:"none"}, "bumble-bee")
+    .from("#Path-18",{duration:2, drawSVG:"0%", ease:"none"}, "bumble-bee")
+    .from("#Path-21",{duration:2, drawSVG:"0%", ease:"none"}, "bumble-bee")
+    .from("#Path-23",{duration:2, drawSVG:"0%", ease:"none"}, "bumble-bee")
+    .from("#Path-24",{duration:2, drawSVG:"0%", ease:"none"}, "bumble-bee")
+    .from("#Path-25",{duration:2, drawSVG:"0%", ease:"none"}, "bumble-bee")
+    .from("#Path-28",{duration:2, drawSVG:"0%", ease:"none"}, "bumble-bee")
 }
